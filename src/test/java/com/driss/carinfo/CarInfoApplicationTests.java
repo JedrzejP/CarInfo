3d5555fd2ce8a5adcb25398f4e929330c0d78bb4
Jedrzej P <jedrzej.panczak@icloud.com>
2023-06-08 11:04:03 +0200

    Commit that shit

---
+++ src/test/java/com/driss/carinfo/CarInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.driss.carinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
